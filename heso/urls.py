51ed5438bb22c66f412b58b3348b66531fb8512a
vladar21 <vladar21@gmail.com>
2024-02-10 22:55:33 +0000

    feat: wire up schedule/templates

--- heso/urls.py
+++ heso/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from scheduling.views import my_schedule
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('schedule/', my_schedule, name='schedule'),
     path('admin/', admin.site.urls),
+    path('', include('scheduling.urls')),
 ]
--- scheduling/views.py
+++ scheduling/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import EnglishClass
 
 
-def my_schedule(request):
-    return HttpResponse("Hello, Schedule!")
+def home(request):
+    classes = EnglishClass.objects.all()  # Getting all english classes
+    return render(request, 'scheduling/home.html', {'classes': classes})
