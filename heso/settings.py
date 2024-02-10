3c06a63c514dff56a09cc2b2a4c20b4d201ceff5
vladar21 <vladar21@gmail.com>
2024-02-10 20:37:19 +0000

    feat: move secret_key to env.py

--- heso/settings.py
+++ heso/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7)9r(-8a45qa2dm8+bp8hxk&jqymh)iv#8ex!h*5^dg1q6oeju'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
