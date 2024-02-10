79f9fa4003cf0b4f4facfe6fdb0e36831cbc0384
vladar21 <vladar21@gmail.com>
2024-02-10 22:17:35 +0000

    feat: register models in schedule admin.py

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import EnglishClass, Schedule, Lesson, Material, GoogleCalendarEvent
 
-# Register your models here.
+
+# Регистрация модели EnglishClass
+@admin.register(EnglishClass)
+class EnglishClassAdmin(admin.ModelAdmin):
+    list_display = ['title', 'teacher']
+    list_filter = ['teacher']
+    search_fields = ['title', 'description']
+
+
+# Регистрация модели Schedule
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ['english_class', 'term', 'start_date', 'end_date']
+    list_filter = ['term', 'start_date', 'end_date']
+
+
+# Регистрация модели Lesson
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['english_class', 'title', 'start_time', 'end_time']
+    list_filter = ['english_class', 'start_time', 'end_time']
+    search_fields = ['title', 'description']
+
+
+# Регистрация модели Material
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type']
+    list_filter = ['type']
+    search_fields = ['title']
+
+
+# Регистрация модели GoogleCalendarEvent
+@admin.register(GoogleCalendarEvent)
+class GoogleCalendarEventAdmin(admin.ModelAdmin):
+    list_display = ['lesson', 'event_time', 'google_event_id']
+    list_filter = ['event_time']
+    search_fields = ['google_event_id']
