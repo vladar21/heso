5fe4c3bad19baff4b59fc26f9d169edf0f7f4dcc
vladar21 <vladar21@gmail.com>
2024-02-12 21:21:21 +0000

    feat: add mont background color for lessons

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+import json
 from .models import Lesson
 
 
 def schedule(request):
-    lessons = Lesson.objects.all()
-    lessons_list = [
-        {
+    lessons = Lesson.objects.prefetch_related('english_class', 'english_class__teacher', 'english_class__students').all()
+    lessons_data = []
+
+    for lesson in lessons:
+        total_lessons = lesson.english_class.lessons.count()
+        lesson_number = list(lesson.english_class.lessons.order_by('start_time')).index(lesson) + 1
+
+        lessons_data.append({
             'id': lesson.id,
-            'title': lesson.title,
-            'start': lesson.start_time.strftime("%Y-%m-%dT%H:%M:%S"),
-            'end': lesson.end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'title': f"{lesson.title} ({lesson_number}/{total_lessons})",
+            'start': lesson.start_time.isoformat(),
+            'end': lesson.end_time.isoformat(),
             'color': lesson.english_class.color,
-            'url': f"/lessons/{lesson.id}/",
-        } for lesson in lessons
-    ]
-    return render(
-        request,
-        'scheduling/schedule.html',
-        {'lessons_list': lessons_list}
-    )
+            'backgroundColor': lesson.english_class.color,
+            'extendedProps': {
+                'teacher': lesson.english_class.teacher.username,
+                'students': list(lesson.english_class.students.values_list('username', flat=True)),
+                'classTitle': lesson.english_class.title
+            }
+        })
+
+    return render(request, 'scheduling/schedule.html', {'lessons_list': json.dumps(lessons_data)})
