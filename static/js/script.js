2a63e2a5be7dd217c7ba4ff0221296d4e1ea8749
vladar21 <vladar21@gmail.com>
2024-03-07 00:35:21 +0000

    fix: readonly for location field in lesson modal's window

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 function showMessage(type, text) {
-    
+
     var container = document.getElementById('messages-container');
     var message = document.createElement('div');
     message.classList.add('alert', 'alert-' + type);
