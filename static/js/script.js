24537692401a301583d87b3ce82c1afa84e5e845
vladar21 <vladar21@gmail.com>
2024-03-07 00:07:54 +0000

    feat: add messages

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+function showMessage(type, text) {
+    
+    var container = document.getElementById('messages-container');
+    var message = document.createElement('div');
+    message.classList.add('alert', 'alert-' + type);
+    message.textContent = text;
+    container.appendChild(message);
+
+    setTimeout(function() {
+        container.removeChild(message);
+    }, 5000);
+}
