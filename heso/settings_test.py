dcf72dc9b88d2c5328c3012f4f9007b51490d793
vladar21 <vladar21@gmail.com>
2024-03-14 23:25:51 +0000

    test: configure tests environment, add first tests

---
+++ heso/settings_test.py
@@ -0,0 +1,10 @@
+# settings_test.py
+
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_test.sqlite3',
+    }
+}
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from .forms import UserRegisterForm
 
-# Create your tests here.
+
+class UserRegisterFormTest(TestCase):
+    def test_form_valid(self):
+        """Test that the user registration form is valid with correct data"""
+        form_data = {'username': 'testuser', 'email': 'user@example.com', 'password1': 'dfgjndfgj34', 'password2': 'dfgjndfgj34'}
+        form = UserRegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_invalid(self):
+        """Test that the user registration form is invalid with incorrect data"""
+        form_data = {'username': 'testuser', 'email': 'user', 'password1': 'password', 'password2': 'password'}
+        form = UserRegisterForm(data=form_data)
+        self.assertFalse(form.is_valid())
