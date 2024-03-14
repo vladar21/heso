e9fba86910f1b43894f679ddc78692946b4afa5f
vladar21 <vladar21@gmail.com>
2024-03-14 23:36:12 +0000

    test: completed tests for users app

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from .forms import UserRegisterForm
+from django.core import mail
+from .models import User
 
 
 class UserRegisterFormTest(TestCase):
@@ -15,3 +17,42 @@ class UserRegisterFormTest(TestCase):
         form_data = {'username': 'testuser', 'email': 'user', 'password1': 'password', 'password2': 'password'}
         form = UserRegisterForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+
+class UserRegistrationTest(TestCase):
+    def test_registration_page_status_code(self):
+        response = self.client.get('/users/register/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_registration_form(self):
+        response = self.client.post(reverse('register'), data={
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+        })
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.status_code, 302)  # Redirect to login page
+        self.assertEqual(len(mail.outbox), 1)  # Check if an email has been sent
+
+
+class LogoutTest(TestCase):
+    def test_logout_redirect(self):
+        response = self.client.get('/users/logout/')
+        self.assertEqual(response.status_code, 302)  # Check redirection to login page
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword123')
+
+    def test_login_page_status_code(self):
+        response = self.client.get('/users/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_form_valid(self):
+        response = self.client.post(reverse('login'), data={
+            'username': 'testuser',
+            'password': 'testpassword123',
+        })
+        self.assertEqual(response.status_code, 302)  # Check redirection after login
