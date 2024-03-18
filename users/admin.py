9fc8838cb8353bed7baea1012cdbf4cdbb5a98a5
vladar21 <vladar21@gmail.com>
2024-03-18 03:33:23 +0000

    main: clean admin.py file

--- users/admin.py
+++ users/admin.py
@@ -7,17 +7,3 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'is_teacher', 'is_student']
     # You can add more customizations here
-
-
-# # Custom admin class for Teacher
-# @admin.register(Teacher)
-# class TeacherAdmin(admin.ModelAdmin):
-#     list_display = ['username', 'department', 'bio']
-#     # Additional customizations
-
-
-# # Custom admin class for Student
-# @admin.register(Student)
-# class StudentAdmin(admin.ModelAdmin):
-#     list_display = ['username', 'enrollment_date', 'major']
-#     # Further customizations
