8a49afcb18353b748d81733a6cd677f5aefcc4e9
vladar21 <vladar21@gmail.com>
2024-03-19 21:01:54 +0000

    docs: add docstrings to users/apps.py

--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,14 @@
+# users/apps.py
+
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    Configuration class for the 'users' application.
+
+    This class configures the 'users' application, specifying
+    the default auto field type and the application name.
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
